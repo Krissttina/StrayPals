779f87a24c0bca98634d3b3ccae41048033cc5e0
Kristina <krismetodieva123@gmail.com>
2024-05-11 13:33:32 +0300

    style update

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 
 function update () {
     var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.querySelector('.filled').style.width = scrolled + "%";
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.querySelector('.filled').style.width = scrolled + "%";
     requestAnimationFrame(update);
 }
 
